5381b0ef749536842966a58258b1f9008a8373f3
afiyakhalid <155869881+afiyakhalid@users.noreply.github.com>
2025-09-23 19:40:00 +0530

    finally resolved timezone issue

--- store/src/main/java/com/code/store/StoreApplication.java
+++ store/src/main/java/com/code/store/StoreApplication.java
@@ -2,12 +2,22 @@ package com.code.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// AFTER
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class StoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StoreApplication.class, args);
-	}
+//    @PostConstruct
+//    public void init() {
+//
+//        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+//        System.out.println("Spring application running in Asia/Kolkata timezone");
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreApplication.class, args);
+    }
 
 }
